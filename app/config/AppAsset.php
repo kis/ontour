a8dc21714302746d676ce3ef1c98356b4dffb5c7
kirillstyopkin <kirillstyopkin@gmail.com>
2014-01-18 21:51:49 +0200

    Use leaflet. Closes #30

--- app/config/AppAsset.php
+++ app/config/AppAsset.php
@@ -17,30 +17,40 @@ class AppAsset extends AssetBundle
 {
 	public $basePath = '@wwwroot';
 	public $baseUrl = '@www';
-	public $css = array(
-        'groundwork/css/groundwork-core.css',
-        'groundwork/css/groundwork-type.css',
-        'groundwork/css/groundwork-ui.css',
-        'groundwork/css/groundwork-anim.css',
-        'groundwork/css/groundwork-ie.css',
-        'css/site.css',
-        'css/lib/animate.css'
-	);
-	public $js = array(
-        'groundwork/js/libs/jquery-1.10.2.min.js',
-        'groundwork/js/groundwork.all.js',
-        'groundwork/js/libs/modernizr-2.6.2.min.js',
 
-        'js/lib/jquery-2.0.3.min.js',
-        'js/lib/jquery-2.0.3.min.map',
-        'js/lib/lastfm.api.js',
-        'js/lib/lastfm.api.cache.js',
-        'js/lib/md5-min.js',
-        'js/vk.js',
-        'js/lastfm.js',
-        'js/lastfm_auth.js',
-        'js/map.js'
+	public $css = array(
+                'groundwork/css/groundwork-core.css',
+                'groundwork/css/groundwork-type.css',
+                'groundwork/css/groundwork-ui.css',
+                'groundwork/css/groundwork-anim.css',
+                'groundwork/css/groundwork-ie.css',
+
+                'leaflet/leaflet.css',
+
+                'css/site.css',
+                'css/lib/animate.css'
 	);
+
+	public $js = array(
+                'groundwork/js/libs/jquery-1.10.2.min.js',
+                'groundwork/js/groundwork.all.js',
+                'groundwork/js/libs/modernizr-2.6.2.min.js',
+
+                'leaflet/leaflet.js',
+
+                'js/lib/jquery-2.0.3.min.js',
+                'js/lib/jquery-2.0.3.min.map',
+
+                'js/lib/lastfm.api.js',
+                'js/lib/lastfm.api.cache.js',
+                'js/lib/md5-min.js',
+
+                'js/vk.js',
+                'js/lastfmLea.js', //lastfmGoogle
+                'js/mapLeaflet.js', //mapGoogle
+                'js/lastfm_auth.js'
+	);
+        
 	public $depends = array(
 		//'yii\web\YiiAsset',
 		//'yii\bootstrap\ResponsiveAsset',
