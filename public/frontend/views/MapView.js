a4ee7f2c071b824380ede17aebbd7fde9f64d5e5
Kirill Styopkin <kirillstyopkin@gmail.com>
2014-03-25 15:40:55 +0200

    Set example map view

--- public/frontend/views/MapView.js
+++ public/frontend/views/MapView.js
@@ -11,7 +11,9 @@ define(['underscore', 'backbone', 'leaflet'], function(_, Backbone) {
 			this.model.set('map', L.map('map-canvas').setView([0, 0], 2))
 					  .get('map').zoomControl.setPosition('bottomright');
 
-			L.tileLayer('http://{s}.tiles.mapbox.com/v3/kirillstyopkin.h29f88g0/{z}/{x}/{y}.png', {
+    	    //'http://{s}.tiles.mapbox.com/v3/kirillstyopkin.h29f88g0/{z}/{x}/{y}.png'
+
+			L.tileLayer('http://api.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png', {
 			    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 			    maxZoom: 18
 			}).addTo(this.model.get('map'));
