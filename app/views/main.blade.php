fd22b8b1140538de3dcc1f6dcc34289c0f009cf9
Kirill Styopkin <kirillstyopkin@gmail.com>
2014-04-01 11:59:29 +0300

    Put all search elements to sidebar

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -12,22 +12,26 @@
 
     <body>
 
-        <div id="search-box">
-            <div id="tabs">
-                <a class="tab active" id="artist-tab">By artist</a>
-                <a class="tab" id="city-tab">By country/city</a>
+        <div id="sidebar">
+
+            <div id="search-box">
+                <div id="tabs">
+                    <a class="tab active" id="artist-tab">By artist</a>
+                    <a class="tab" id="city-tab">By country/city</a>
+                </div>
+
+                <input id="artist-field" class="search-field" placeholder="Enter artist name..">                 
+
+                <div id="autocomplete"></div>
+
+                <a id="artist-button" class="search-button">Search</a>
             </div>
 
-            <input id="artist-field" class="search-field" placeholder="Enter artist name..">                 
+            <div id="artist-info">
 
-            <div id="autocomplete"></div>
+                <div class="info-block"></div>
 
-            <a id="artist-button" class="search-button">Search</a>
-        </div>
-
-        <div id="artist-info">
-
-            <div class="info-block"></div>
+            </div>
 
         </div>
 
