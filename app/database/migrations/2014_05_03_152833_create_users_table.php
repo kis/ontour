1db7acaa042f1e96b14730100ab2a713cb524a2f
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-05-03 18:50:52 +0300

    Add users migration

---
+++ app/database/migrations/2014_05_03_152833_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('users', function($table)
+        {
+            $table->increments('id');
+            $table->string('login');
+            $table->string('password');
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('sex');
+            $table->string('location');
+            $table->string('phone');
+            $table->string('photo');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('users');
+	}
+
+}
