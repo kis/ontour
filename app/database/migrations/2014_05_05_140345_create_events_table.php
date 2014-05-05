f0d7488b54aaef2b0847c22cf3aef7ea2bafba26
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-05-05 17:42:27 +0300

    Fix events info. Add migrations for events and user_events

---
+++ app/database/migrations/2014_05_05_140345_create_events_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('events', function($table) {
+            $table->integer('event_id');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('events');
+	}
+
+}
