301cde332ab2c16c53f51a033e95bd901ecbeaff
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-05-23 14:24:35 +0300

    Clean code. Get vk audiolist. Closes #9

--- public/js/main.js
+++ public/js/main.js
@@ -29,15 +29,16 @@ requirejs.config({
 	},
 
 	paths: {
-		jquery: 'lib/jquery.min', //'http://code.jquery.com/jquery-latest.min',
-		mapbox: 'lib/mapbox', //'https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox',
-		cluster: 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster',
-		text: 'lib/text',
-		underscore: 'lib/underscore-min',
-		backbone: 'lib/backbone-min',
-		marionette: 'lib/backbone.marionette.min',
-		scrollbar: 'lib/scrollbar.min',
-		mousewheel: 'lib/jquery.mousewheel'
+		jquery 		: 'lib/jquery.min', //'http://code.jquery.com/jquery-latest.min',
+		mapbox 		: 'lib/mapbox', //'https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox',
+		cluster 	: 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster',
+		vk  		: 'http://vkontakte.ru/js/api/openapi', 		
+		text 		: 'lib/text',
+		underscore  : 'lib/underscore-min',
+		backbone    : 'lib/backbone-min',
+		marionette  : 'lib/backbone.marionette.min',
+		scrollbar   : 'lib/scrollbar.min',
+		mousewheel  : 'lib/jquery.mousewheel'
 	}
 });
 
--- public/js/views/SettingsView.js
+++ public/js/views/SettingsView.js
@@ -1,8 +1,9 @@
 define(['channel',
+		'vk',
 		'text', 
 		'text!templates/Settings.tmpl',
 		'marionette'
-], function(channel, text, settingsTmpl) {
+], function(channel, vk, text, settingsTmpl) {
 	'use strict';
 
 	return Marionette.ItemView.extend({
@@ -14,33 +15,23 @@ define(['channel',
 		ui: {
 			logout   : '#logout',
 			profile  : '#profile',
-			myevents : '#myevents'
+			myevents : '#myevents',
+			vk		 : '#vk'
 		},
 
 		events: {
 			'click @ui.logout'   : 'logout',
 			'click @ui.profile'  : 'profile',
-			'click @ui.myevents' : 'myevents'
+			'click @ui.myevents' : 'myevents',
+			'click @ui.vk' 	 	 : 'vk'
 		},
 
 		logout: function() {
-			Backbone.ajax({
-				url: 'users/logout',
-				type: 'GET',
-				success: function() {
-					document.location.replace("users/login-page");
-				}
-			});			
+			document.location.replace("users/logout");
 		},
 
 		profile: function() {
-			Backbone.ajax({
-				url: 'users/profile',
-				type: 'GET',
-				success: function() {
-					document.location.replace("users/profile");
-				}
-			});			
+			document.location.replace("users/profile");
 		},
 
 		myevents: function() {
@@ -72,6 +63,26 @@ define(['channel',
 				success: function(data) {
 					channel.trigger('addEvent', data.event);
 				}	
+			});
+		},
+
+		vk: function() {
+			VK.init({
+				apiId: '3903848'
+			});
+
+			/*VK.Auth.login(function(response) {
+				console.log(response);
+
+				if (response.session) {
+					console.log('user: '+response.session.mid);
+				} else {
+					console.log('not auth');
+				}
+			});*/
+
+			VK.Api.call('audio.get', {uid: '9408031'}, function(response) {
+				console.log(response);
 			});
 		}
 
